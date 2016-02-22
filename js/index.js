6691bcdba370fda098b8b2df492576093bb19eaf
mkgramlow <mariska.gramlow@eagles.ewu.edu>
2016-02-21 19:40:45 -0800

    Base Commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+$('document').ready(function() 
+                    
+                    {
+  var options = { videoId: 'q7o7R5BgWDY',
+                 start: 3,
+                 mute: false
+                
+                };
+	$('#wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+   // cdrHWbBQ-Nc Oil Pump
+   // rnQ7nayIcLo sun sparcles over the river
+});
